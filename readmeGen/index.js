e8f2f66fb1ac22639c5b60c0e47f5f12829e30fe
Andrew An <andyan7@gmail.com>
2022-02-14 12:22:18 -0800

    Prepared key-value entry

---
+++ readmeGen/index.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const readmeGen = ({title, description, table_of_contents, installation, usage, license, contributing, tests, questions})
+
+inquirer.prompt ([
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+])
