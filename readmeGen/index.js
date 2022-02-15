88d34c8a7ed1a3f6034dbf89d6e1f9c53fe8f8b8
Andrew An <andyan7@gmail.com>
2022-02-14 17:27:39 -0800

    Added array of objects

--- readmeGen/index.js
+++ readmeGen/index.js
@@ -1,34 +1,68 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const readmeGen = ({title, description, table_of_contents, installation, usage, license, contributing, tests, questions})
+const readmeGen = ({title, description, installation, usage, license, contributing, questions}) =>
+`
+
+`
 
 inquirer.prompt ([
     {
-
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
     },
     {
-
+        type: 'input',
+        message: 'How would your project be described?',
+        name: 'description',
     },
     {
-
+        type: 'input',
+        message: 'How would a user install your program?',
+        name: 'installation',
     },
     {
-
+        type: 'input',
+        message: 'Which license will be applied to your project?',
+        name: 'license',
     },
     {
-
+        type: 'input',
+        message: 'How will your program be utilized?',
+        name: 'usage',
     },
     {
-
+        type: 'input',
+        message: 'Please name/list contributor(s):',
+        name: 'contribution',
     },
     {
-
+        type: 'input',
+        message: 'Please provide the project gitHub repository link:',
+        name: 'repo',
     },
     {
-
+        name: 'input',
+        message: 'Please provide the deployed application link:',
+        name: 'app'
     },
     {
-
+        name: 'input',
+        message: 'Please provide your gitHub user name:',
+        name: 'authorName',
+    },
+    {
+        name: 'input',
+        message: 'Please provide your contact email address:',
+        name: 'email',
+    },
+    {
+        name: 'input',
+        message: 'Please enter your contact preference for prospective contributors:',
+        name: 'prospect',
+    },
+    {
+        
     },
 ])
