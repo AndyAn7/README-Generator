5185eb9fae83233d4d57278a00b09338fe775fc1
Andrew An <andyan7@gmail.com>
2022-02-19 23:20:32 -0800

    Corrected spacing

--- readmeGen/code.js
+++ readmeGen/code.js
@@ -23,32 +23,32 @@ const rMe = (response) => {
 
 
 ## [Description](#description)
----
+
 <a name="description"></a>
 ${response.description}.
 
 ## [Language Utilized](#language)
----
+
 <a name="language"></a>
 ${response.language}
 
 ## [Utility](#utility)
----
+
 <a name="usage"></a>
 ${response.usage}.
 
 ## [Contributors](#contributors)
----
+
 <a name="contribution"></a>
 ${response.contribution}.
 
 ## [Application Installation](#installation)
----
+
 <a name="installation"></a>
 ${response.installation}.
 
 ## [Inquiries](#inquiries)
----
+
 <a name="inquiries"></a>
 Feel free to inquire in regard to this project:
 
@@ -60,12 +60,12 @@ To become a contributor, contact:
 - ${response.prospect}
 
 ## [Installation](#installation)
----
+
 <a name="installation"></a>
 ${response.installation}
 
 ## [License](#license)
----
+
 <a name="license"></a>
 ${response.license}
 
